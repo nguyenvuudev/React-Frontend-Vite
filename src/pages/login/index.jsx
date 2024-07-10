c16cf67466464f8187ad147d9ae698c019a5b24b
hoangvu17tb <nguyenvuu17tb@gmail.com>
2024-07-10 15:34:34 +0700

    Add_Axios_Customize

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,75 @@
-const LoginPage = () => {
-    return (
-        <>
-            login page
-        </>
-    )
-}
+// import { Button, Form, Input, message, notification } from 'antd'
+// import { Link, useNavigate } from 'react-router-dom'
+// import { useState } from 'react'
+// import './login.scss'
+// import '../../index.css'
+// import { callLogin } from '../../services/api'
 
-export default LoginPage;
+// const LoginPage = () => {
+//     const navigate = useNavigate()
+//     const [isSubmit, setIsSubmit] = useState(false)
+
+//     const onFinish = async (values) => {
+//         const { username, password } = values
+//         setIsSubmit(true)
+//         const res = await callLogin(username, password)
+//         setIsSubmit(false)
+//         if (res?.data) {
+//             message.success('Đăng nhập tài khoản thành công!')
+//             navigate('/')
+//         } else {
+//             message.error('Thông tin đăng nhập chưa chính xác')
+//             navigate('/login')
+//         }
+//     }
+
+//     return (
+//         <div className="container-login">
+//             <div className="wrapper-login">
+//                 <div className="title-login">
+//                     Đăng nhập
+//                 </div>
+//                 <div className="form-control">
+//                     <Form
+//                         name="basic"
+//                         onFinish={onFinish}
+//                         autoComplete="off"
+//                     >
+
+//                         <Form.Item
+//                             labelCol={{ span: 24 }}
+//                             label="Email"
+//                             name="username"
+//                             rules={[{ required: true, message: 'Email không được để trống!' }]}
+//                         >
+//                             <Input />
+//                         </Form.Item>
+
+//                         <Form.Item
+//                             labelCol={{ span: 24 }}
+//                             label="Mật khẩu"
+//                             name="password"
+//                             rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
+//                         >
+//                             <Input.Password />
+//                         </Form.Item>
+
+//                         <Form.Item  >
+//                             <Button className='btn-login' type="primary" htmlType="submit" loading={isSubmit}>
+//                                 Đăng nhập
+//                             </Button>
+//                         </Form.Item>
+//                         <p className='text-login'>Chưa có tài khoản ?
+//                             <a>
+//                                 <Link to='/register' > Đăng ký </Link>
+//                             </a>
+//                         </p>
+//                     </Form>
+
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default LoginPage
---
+++ src/utils/axios-customize.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+
+const baseBackendURL = import.meta.env.VITE_BACKEND_URL
+
+const instance = axios.create({
+  baseURL: baseBackendURL,
+})
+
+instance.interceptors.request.use(
+  function (config) {
+    return config
+
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+instance.interceptors.response.use(
+  function (response) {
+    console.log(response);
+    return response && response.data ? response.data : response
+    
+  }, function (error) {
+    return error?.response?.data ?? Promise.reject(error)
+  })
+
+export default instance
