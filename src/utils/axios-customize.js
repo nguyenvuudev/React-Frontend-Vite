2284b0085d3cfb5996ab2375179d9e93c378ecb9
hoangvu17tb <nguyenvuu17tb@gmail.com>
2024-08-12 08:44:48 +0700

    Retry_Handle_Refresh_Token

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -10,8 +10,19 @@ const instance = axios.create({
 // Sending the bearer token with axios
 instance.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem('access_token')}` }
 
+const handleRefreshToken = async () => {
+  const res = await instance.get('/api/v1/auth/refresh')
+  if (res && res.data) return res.data.access_token
+  else null
+
+  // console.log('===check res: ', res); 
+}
+
+const NO_RETRY_HEADER = 'x-no-retry'
+
 instance.interceptors.request.use(
   function (config) {
+    // console.log('===check config: ',config);
     return config
 
   }, function (error) {
@@ -20,10 +31,27 @@ instance.interceptors.request.use(
 
 instance.interceptors.response.use(
   function (response) {
-    console.log(response);
+    // console.log('===check response: ', response);
     return response && response.data ? response.data : response
 
-  }, function (error) {
+  },
+  
+  // Retry (gọi lại API khi hết hạn hoặc bị lỗi)
+  async function (error) {
+    if (error.config
+      && error.response
+      && +error.response.status === 401
+      && !error.config.headers[NO_RETRY_HEADER]
+    ) {
+      const access_token = await handleRefreshToken()
+      error.config.headers[NO_RETRY_HEADER] = 'true'
+      if (access_token) {
+        error.config.headers['Authorization'] = `Bearer ${access_token}`
+        localStorage.setItem('access_token', access_token)
+        return instance.request(error.config)
+      }
+    }
+
     return error?.response?.data ?? Promise.reject(error)
   })
 
