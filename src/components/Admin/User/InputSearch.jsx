118d0766acf99451af7822bacd6b97148f6143e9
hoangvu17tb <nguyenvuu17tb@gmail.com>
2024-08-20 23:22:18 +0700

    Add_Read_File_Excel

--- src/components/Admin/User/InputSearch.jsx
+++ src/components/Admin/User/InputSearch.jsx
@@ -13,68 +13,69 @@ const InputSearch = (props) => {
     }
 
     const onFinish = (values) => {
-       let query = ""
-      if (values.fullName) {
-        query += `&fullName=/${values.fullName}/i` // lấy tương đối
-      }
-      if (values.email) {
-        query += `&email=/${values.email}/i`
-      }
-      if (values.phone) {
-        query += `&phone=/${values.phone}/i`
-      }
-      if (query) {
-        props.handleSearch(query)
-      }
+        let query = ""
+        if (values.fullName) {
+            query += `&fullName=/${values.fullName}/i` // lấy tương đối
+        }
+        if (values.email) {
+            query += `&email=/${values.email}/i`
+        }
+        if (values.phone) {
+            query += `&phone=/${values.phone}/i`
+        }
+        if (query) {
+            props.handleSearch(query)
+        }
     }
 
     return (
-        <Form form={form} name="advanced_search" style={formStyle} onFinish={onFinish}>
-            <Row gutter={24}>
-                <Col span={8}>
-                    <Form.Item
-                        labelCol={{ span: 24 }} //whole column
-                        name={`fullName`}
-                        label={`Tên`}
-                    >
-                        <Input placeholder="tên..." />
-                    </Form.Item>
-                </Col>
-                <Col span={8}>
-                    <Form.Item
-                        labelCol={{ span: 24 }} //whole column
-                        name={`email`}
-                        label={`Email`}
-                    >
-                        <Input placeholder="email..." />
-                    </Form.Item>
-                </Col>
+        <>
+            <Form form={form} name="advanced_search" style={formStyle} onFinish={onFinish}>
+                <Row gutter={24}>
+                    <Col span={8}>
+                        <Form.Item
+                            labelCol={{ span: 24 }} //whole column
+                            name={`fullName`}
+                            label={`Tên`}
+                        >
+                            <Input placeholder="tên..." />
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item
+                            labelCol={{ span: 24 }} //whole column
+                            name={`email`}
+                            label={`Email`}
+                        >
+                            <Input placeholder="email..." />
+                        </Form.Item>
+                    </Col>
 
-                <Col span={8}>
-                    <Form.Item
-                        labelCol={{ span: 24 }} //whole column
-                        name={`phone`}
-                        label={`Số điện thoại`}
-                    >
-                        <Input placeholder="số điện thoại..." />
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row>
-                <Col span={24} style={{ textAlign: 'right' }}>
-                    <Button type="primary" htmlType="submit">
-                        Tìm kiếm
-                    </Button>
-                    <Button
-                        style={{ margin: '0 8px' }}
-                        onClick={() => {
-                            form.resetFields()
-                            props.setFilter()
-                        }}
-                    >
-                        Xóa
-                    </Button>
-                    {/* <a
+                    <Col span={8}>
+                        <Form.Item
+                            labelCol={{ span: 24 }} //whole column
+                            name={`phone`}
+                            label={`Số điện thoại`}
+                        >
+                            <Input placeholder="số điện thoại..." />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24} style={{ textAlign: 'right' }}>
+                        <Button type="primary" htmlType="submit">
+                            Tìm kiếm
+                        </Button>
+                        <Button
+                            style={{ margin: '0 8px' }}
+                            onClick={() => {
+                                form.resetFields()
+                                props.setFilter()
+                            }}
+                        >
+                            Xóa
+                        </Button>
+                        {/* <a
                         style={{ fontSize: 12 }}
                         onClick={() => {
                             setExpand(!expand)
@@ -82,9 +83,10 @@ const InputSearch = (props) => {
                     >
                         {expand ? <UpOutlined /> : <DownOutlined />} Collapse
                     </a> */}
-                </Col>
-            </Row>
-        </Form>
+                    </Col>
+                </Row>
+            </Form>
+        </>
     )
 }
 
--- src/components/Admin/User/data/UserImport.jsx
+++ src/components/Admin/User/data/UserImport.jsx
@@ -1,19 +1,20 @@
 import { InboxOutlined } from '@ant-design/icons'
-import { Modal, Table, Upload } from 'antd'
-import React from 'react'
+import { message, Modal, Table, Upload } from 'antd'
+import React, { useState } from 'react'
+import * as XLSX from 'xlsx'
 
 const { Dragger } = Upload
 
 const UserImport = (props) => {
   const { openModalImport, setOpenModalImport } = props
 
+  const [dataExcel, setDataExcel] = useState([])
+
   const dummyRequest = ({ file, onSuccess }) => { // giả định tải file lên thành công
     setTimeout(() => {
       onSuccess("ok")
     }, 1000)
   }
-
-
 
   const propsUpload = {
     name: 'file',
@@ -22,12 +23,34 @@ const UserImport = (props) => {
     maxCount: 1,
     customRequest: dummyRequest,
     onChange(info) {
-      console.log("===info: ", info)
+      console.log("=== check info: ", info)
       const { status } = info.file
+      console.log('nvd_checkStatus: ', status)
       if (status !== 'uploading') {
         console.log(info.file, info.fileList)
       }
       if (status === 'done') {
+        if (info.fileList && info.fileList.length > 0) {
+          const file = info.fileList[0].originFileObj
+          const reader = new FileReader()
+
+          // reader.onload is method of FileReader()
+          reader.onload = function (e) { // call back function
+            const data = new Uint8Array(reader.result) // Chuyển đổi sữ liệu của file sang dạng số nhị phân
+            const workBook = XLSX.read(data, { type: 'array' }) // Chuyển dữ liệu vào thư viện
+            const wokSheet = workBook.Sheets[workBook.SheetNames[0]] // Lấy vị trí sheet đầu tiên trong excel
+
+            const jsonData = XLSX.utils.sheet_to_json(wokSheet, { // Chuyển sang định dạng json
+              header: ["fullName", "email", "phone"],
+              range: 1 // bỏ qua hàng đầu tiên của bảng
+            })
+            if (jsonData && jsonData.length > 0) {
+              setDataExcel(jsonData)
+            }
+          }
+          reader.readAsArrayBuffer(file) 
+        }
+
         message.success(`${info.file.name} file uploaded successfully.`)
       } else if (status === 'error') {
         message.error(`${info.file.name} file upload failed.`)
@@ -80,6 +103,7 @@ const UserImport = (props) => {
           <Table
             title={() => <span>Dữ liệu tải lên:</span>}
             columns={columns}
+            dataSource={dataExcel}
           >
           </Table>
         </div>
